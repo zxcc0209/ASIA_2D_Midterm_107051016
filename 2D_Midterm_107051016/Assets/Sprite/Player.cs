bd2a66348f25a77dc497ecf06ef3db762fd8c8e5
zxcc0209 <71679895+zxcc0209@users.noreply.github.com>
2020-11-15 05:14:23 +0800

    Finish

---
+++ 2D_Midterm_107051016/Assets/Sprite/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //修飾詞 類別 名稱
+    //None 空直-程式錯誤
+    public GameObject Final;
+    public Text textcount;
+    public int count;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送們了");
+
+        if (collision.name == "傳送門")
+        {
+            Final.SetActive(true);
+        }
+
+        if (collision.tag == "gem")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textcount.text = count + "/99";
+        }
+
+    }
+
+}
